0f96104cffc427594e9b2faea3079287dc501116
github username <github email address>
2017-03-23 18:13:32 -0700

    Done.

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,10 @@
 # Create your CostumeStore class here
+# has a name
+# has a price
+# has an image url
+# has a size
+# knows when it was created
+# knows when it was last updated
+
+class CostumeStore < ActiveRecord::Base
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,23 @@
 # Create your haunted_houses migration here
+# has a name (FAILED - 1)
+# has a location (FAILED - 2)
+# has a theme (FAILED - 3)
+# has a price (FAILED - 4)
+# knows if it's family friendly (FAILED - 5)
+# has an opening date (FAILED - 6)
+# has a closing date (FAILED - 7)
+# has a long, long description (FAILED - 8)
+class CreateHauntedHouses < ActiveRecord::Migration
+  def change
+    create_table :haunted_houses do|t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.integer :price
+      t.boolean :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.text :description
+    end
+  end
+end
